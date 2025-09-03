dff4226b45d759aa2c951c113bf3b7505275c021
Mateus Felipe <brasil.mateus748@gmail.com>
2025-09-03 10:10:58 -0300

    First commit

---
+++ src/test/java/com/project/customerportal/IspCustomerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.customerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IspCustomerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
