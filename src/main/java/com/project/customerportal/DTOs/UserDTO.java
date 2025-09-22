f1db6bdf7d11bd604034306ed5074f0043b3bb2f
Mateus Felipe <brasil.mateus748@gmail.com>
2025-09-21 23:54:30 -0300

    Changed the fields Added the getters and setters

--- src/main/java/com/project/customerportal/DTOs/UserDTO.java
+++ src/main/java/com/project/customerportal/DTOs/UserDTO.java
@@ -1,13 +1,29 @@
 package com.project.customerportal.DTOs;
 
+import lombok.Getter;
+
+@Getter
 public class UserDTO {
-    public long Id;
-    public String Name;
+    private String Login;
+    private String Password;
+
     public boolean status;
 
-    public UserDTO(long id, String name, boolean status) {
-        Id = id;
-        Name = name;
+    public UserDTO(String login, String password, boolean status) {
+        Login = login;
+        Password = password;
+        this.status = status;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void setStatus(boolean status) {
         this.status = status;
     }
 }
