bdfee242140904af0376fb9dcb806fb9fa016cdc
Mateus Felipe <brasil.mateus748@gmail.com>
2025-10-07 11:16:17 -0300

    Added user controller

---
+++ src/main/java/com/project/customerportal/Controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.project.customerportal.Controllers;
+
+import com.project.customerportal.Entity.User;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    public List<User> getAllUsers() {
+    };
+}
