d5929753eb4f0333edce94a34126cdd6a4867807
Mateus Felipe <brasil.mateus748@gmail.com>
2025-10-25 23:55:52 -0300

    Added method to create a user in the service layer

--- src/main/java/com/project/customerportal/Service/UserService.java
+++ src/main/java/com/project/customerportal/Service/UserService.java
@@ -1,18 +1,25 @@
 package com.project.customerportal.Service;
 
 import com.project.customerportal.DTOs.UserDTO;
+import com.project.customerportal.Entity.User;
 import com.project.customerportal.Repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 
 public class UserService {
     private final UserRepository userRepository;
 
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public ResponseEntity<UserDTO> createUser (long Id) {
-        if(user)
-            return userRepository.
+    public User createUser (UserDTO userdto, long id) {
+        if() {
+            throw new RuntimeException("User already exists!");
+        }
+        User user = new User();
+        user.setLogin(userdto.getLogin());
+        user.setPassword(userdto.getPassword());
+        return userRepository.save(user);
     }
 }
